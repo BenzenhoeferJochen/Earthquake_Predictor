cea5010b92d97c49adc72c9e5951dcfa9464067b
Jochen Benzenhöfer <jochen.benzenhoefer@it-tem.de>
2025-07-17 14:39:53 +0200

    tempfile for Node Red

--- file.tf
+++ file.tf
@@ -8,10 +8,23 @@ resource "local_file" "nodeRed_settings" {
 
 resource "local_file" "nodeRed_flows" {
   content = templatefile("${path.module}/Node-Red-Template/flows.json", {
-    db_address        = aws_db_instance.Node_Red_DB.address,
-    db_port           = aws_db_instance.Node_Red_DB.port,
-    db_database       = var.DB_DATABASE
+    db_address  = aws_db_instance.Node_Red_DB.address,
+    db_port     = aws_db_instance.Node_Red_DB.port,
+    db_database = var.DB_DATABASE,
+    backendIP   = aws_instance.Python_ML_API_Server.public_ip
 
   })
   filename = "${path.module}/Node-Red/flows.json"
+}
+
+resource "local_file" "backup_vars" {
+  filename = "${path.module}/tmp/backup_vars.json"
+
+  content = jsonencode({
+    credentialSecret = var.CREDENTIAL_SECRET,
+    db_database      = var.DB_DATABASE,
+    db_port          = aws_db_instance.Node_Red_DB.port,
+    db_address       = aws_db_instance.Node_Red_DB.address,
+    backendIP        = aws_instance.Python_ML_API_Server.public_ip
+  })
 }
