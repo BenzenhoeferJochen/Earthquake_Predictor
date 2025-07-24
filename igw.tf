2e23a26ff51d40701b1c5c1577b8dba851296a3d
Jochen Benzenhöfer <jochen.benzenhoefer@it-tem.de>
2025-07-24 14:34:54 +0200

    Make the private Subnets private

--- igw.tf
+++ igw.tf
@@ -16,10 +16,3 @@ resource "aws_route" "IGW_Route" {
   gateway_id             = aws_internet_gateway.IGW.id
 }
 
-
-# Add Route to Route Table
-resource "aws_route" "NAT_Route" {
-  route_table_id         = aws_route_table.Private_Route_Table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.IGW.id
-}
